7b3716303f0a8318fbbdbe224a00f40bb5786b0f
ladyada <limor@ladyada.net>
2014-07-04 14:26:51 -0400

    init

---
+++ examples/MMA8451demo/MMA8451demo.ino
@@ -0,0 +1,64 @@
+/**************************************************************************/
+/*!
+    @file     Adafruit_MMA8451.h
+    @author   K. Townsend (Adafruit Industries)
+    @license  BSD (see license.txt)
+
+    This is an example for the Adafruit MMA8451 Accel breakout board
+    ----> https://www.adafruit.com/products/
+
+    Adafruit invests time and resources providing this open source code,
+    please support Adafruit and open-source hardware by purchasing
+    products from Adafruit!
+
+    @section  HISTORY
+
+    v1.0  - First release
+*/
+/**************************************************************************/
+
+#include <Wire.h>
+#include <Adafruit_MMA8451.h>
+#include <Adafruit_Sensor.h>
+
+Adafruit_MMA8451 mma = Adafruit_MMA8451();
+
+void setup(void) {
+  Serial.begin(9600);
+  
+  Serial.println("MMA8451 test!");
+  
+
+  if (! mma.begin()) {
+    Serial.println("Couldnt start");
+    while (1);
+  }
+  Serial.println("MMA8451 found!");
+  
+  mma.setRange(MMA8451_RANGE_8_G);
+    mma.writeRegister8(MMA8451_REG_CTRL_REG4, 0x01);
+  
+  Serial.print("Range = "); Serial.print(2 << mma.getRange());  
+  Serial.println("G");
+  
+}
+
+void loop() {
+  mma.read();
+  Serial.print("X:\t"); Serial.print(mma.x); 
+  Serial.print("\tY:\t"); Serial.print(mma.y); 
+  Serial.print("\tZ:\t"); Serial.print(mma.z); 
+  Serial.println();
+
+  /* Get a new sensor event */ 
+  sensors_event_t event; 
+  mma.getEvent(&event);
+
+  /* Display the results (acceleration is measured in m/s^2) */
+  Serial.print("X: "); Serial.print(event.acceleration.x); Serial.print("  ");
+  Serial.print("Y: "); Serial.print(event.acceleration.y); Serial.print("  ");
+  Serial.print("Z: "); Serial.print(event.acceleration.z); Serial.print("  ");Serial.println("m/s^2 ");
+  
+  delay(500);
+  
+}
